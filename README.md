3ce44dd6aa76c7d7b3e49731f0722f6458fd197a
unknown <darrenheath23@hotmail.com>
2022-09-23 10:38:05 +0200

    commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Week-3-Circle-Website
+
+Finished HTML of front page and did some styling
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      maxWidth: {
+        '1/2': '50%',
+      }
+    },
+  },
+  plugins: [],
+}
